0674741f0bb02bc89c3a35aa771e09c800570316
wood-charoal <wuyurui@whu.edu.cn>
2025-11-23 20:18:26 +0800

    update clean scripts

--- clean.sh
+++ clean.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-rm 9*.loop
+# Clean up log files
+rm *.log
 
-rm -rf cpp/build/*
+# Clean up build directory
+rm -rf ./cpp/build
 
-mkdir -p cpp/build/googletest
-unzip ../googletest-release-1.8.0.zip -d cpp/build/googletest
+# Clean up distribution directory
+rm -rf ./dist
+
+# Recreate build directory
+mkdir -p ./cpp/build/googletest
+unzip ../googletest-release-1.8.0.zip -d ./cpp/build/googletest
